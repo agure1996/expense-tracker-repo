9ae01a74165a001cd1c7e1d0ca6d6aeb3185c697
A <abbasgure@gmail.com>
2024-05-22 14:02:33 +0100

    added repository

---
+++ src/main/java/com/example/agure1996/expense/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.agure1996.expense.repositories;
+
+import com.example.agure1996.expense.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
+}
