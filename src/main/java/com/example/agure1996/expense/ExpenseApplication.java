b9474878b1b39f8e8c579a24d8eb97b001607f77
A <abbasgure@gmail.com>
2024-05-22 13:21:18 +0100

    First commit

---
+++ src/main/java/com/example/agure1996/expense/ExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.agure1996.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApplication.class, args);
+	}
+
+}
